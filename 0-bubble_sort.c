a48d3b631607a8db366ea2f46369b209876cc612
Alhadji Oumate <alhadjioum0@gmail.com>
2023-11-20 12:38:50 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array using the bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size fo the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp, swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		
+		if (swapped == 0)
+			break;
+	}
+}
