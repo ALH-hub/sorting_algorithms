8d025e8446cec35c791c77a1fead0e03286defc7
Alhadji Oumate <alhadjioum0@gmail.com>
2023-11-20 15:09:37 +0100

    insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two node on a Dlist.
+ * @list: head of the Dlist
+ * @current: Current node
+ * @previous: previous node
+ */
+void swap_nodes(listint_t **list, listint_t *current, listint_t *previous)
+{
+	if (current->next)
+		(current->next)->prev = previous;
+
+	current->prev = previous->prev;
+	current->next = previous;
+
+	if (previous->prev)
+		(previous->prev)->next = current;
+	else
+		(*list) = current;
+}
+
+/**
+ * insertion_sort_list - sort a DLlist using insertion sort algo
+ * @list: Doubly linked list of integers(Dlist)
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *previous = NULL, *current = NULL, *next = NULL;
+
+	if (!list || !(*list))
+		return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		next = current->next;
+		previous = current->prev;
+
+		while (previous && current->n < previous->n)
+		{
+			previous->next = current->next;
+
+			swap_nodes(list, current, previous);
+			previous->prev = current;
+			previous = current->prev;
+
+			print_list(*list);
+		}
+		current = next;
+	}
+}
--- sort.h
+++ sort.h
@@ -20,6 +20,8 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *current, listint_t *previous);
 void print_list(const listint_t *list);
 
 #endif
